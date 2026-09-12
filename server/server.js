302e2706443d163bdc0b1ba488ef439ea7839898
sudha <s13809297@gmail.com>
2026-09-12 10:58:16 +0530

    Use Render assigned port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const resumeRoutes = require("./routes/resumeRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
